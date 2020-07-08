d36446a1808a4d164aa8e2e474c44ce529187320
kewin <404502753@qq.com>
2020-07-08 23:20:17 +0800

    vue+typescript搭建后台管理界面

---
+++ admin/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script lang="ts">
+// Vue属性的装饰器
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class App extends Vue {}
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
