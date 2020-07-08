6e7d06ec92a68fe593c57b48826d3d180e03eb94
JTPeng <404502753@qq.com>
2020-07-08 11:00:49 +0800

    swagger接口文档搭建

--- server/libs/db/src/db.module.ts
+++ server/libs/db/src/db.module.ts
@@ -1,8 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { DbService } from './db.service';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { User } from './models/user.model';
 
+
+const models = TypegooseModule.forFeature([User])
+@Global()  // db模块为全局模块
 @Module({
+  imports: [
+    TypegooseModule.forRoot('mongodb://localhost/topfullstack', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: true,
+    }),
+    models
+  ],
   providers: [DbService],
-  exports: [DbService],
+  exports: [DbService, models],
 })
-export class DbModule {}
+export class DbModule { }
